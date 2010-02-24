9d940de27ad5e7305712ebd4669b8697a661a9b0
Stephen Woods <saw@yahoo-inc.com>
2010-02-24 15:58:28 -0800

    save state for now

--- bin/engine.js
+++ bin/engine.js
@@ -1,5 +1,19 @@
+var fs = require('fs'),
+conf   = require('../modules/conf'),
+sys    = require('sys'),
+url    = require('url');
+
+var api = {
+	
+	getConf:conf.getConf
+	
+};
+
+
 function handleRequest(request, response, callback){
 	
+	var path = url.parse(request.url).pathname;
+	sys.puts(path);
 	response.writeHeader(200, {'Content-Type':'text/html'});
 	response.write('This is great');
 	
